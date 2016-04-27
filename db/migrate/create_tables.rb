e616a0c4149ddf52d38a8a793270eb0538457b47
Martin Laws <martin.laws@thinkresearch.com>
2016-04-27 18:51:22 -0400

    Created models and tables

--- db/migrate/create_tables.rb
+++ db/migrate/create_tables.rb
@@ -1,9 +1,19 @@
-class Create? < ActiveRecord::Migration
+class CreateTables < ActiveRecord::Migration
   def change
-    create_table :? do |t|
-      t.string :?
-      t.text :?
-      t.timestamps
+    create_table :users do |column|
+      column.string :name
+      column.string :photo_url
+      column.integer :gold_stars
+      column.integer :black_squares
+      column.integer :class_id
+      column.timestamps
+    end
+    create_table :classrooms do |t|
+      t.string :nickname
+      t.date :start_date
+      t.date :end_date
+      t.string :class_type
+      t.string :topic
     end
   end
 end
